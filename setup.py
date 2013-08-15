32dbb8ef84e49ad500676c2354f775a486abddb7
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-08-14 23:36:33 -0400

    made soft links for README.md to README and README.txt

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='prettyplotlib',
-    version='0.1.1',
+    version='0.1.2',
     author='Olga B. Botvinnik',
     author_email='olga.botvinnik@gmail.com',
     packages=['prettyplotlib'],
     license='LICENSE.txt',
     url='http://github.com/olgabot/prettyplotlib',
     description='Painlessly create beautiful default `matplotlib` plots.',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
     install_requires=['matplotlib >= 1.2.1',
               'brewer2mpl >= 1.3.1']
 )
