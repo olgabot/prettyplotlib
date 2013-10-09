7a4e6c678f1b2a0330da60526464a266b4b15e62
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-10-08 21:51:29 -0700

    outward ticks, and show_ticks is at least trying to work

--- prettyplotlib/__init__.py
+++ prettyplotlib/__init__.py
@@ -431,11 +431,12 @@ def remove_chartjunk(ax, spines, grid=None, ticklabels=None, show_ticks=False):
         axis = ax.__dict__[ax_name]
         # axis.set_tick_params(color=almost_black)
         print 'axis.get_scale()', axis.get_scale()
-        if axis.get_scale() == 'log':
+        if show_ticks or axis.get_scale() == 'log':
             # if this spine is not in the list of spines to remove
             for p in pos.difference(spines):
                 print 'p', p
                 axis.set_ticks_position(p)
+                axis.set_tick_params(direction='out')
                 #                axis.set_tick_params(which='both', p)
         else:
             axis.set_ticks_position('none')
