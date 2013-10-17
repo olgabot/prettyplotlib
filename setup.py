062e2c847d10e4cf32870295f66e9b410c85bac5
Chris Lasher <chris.lasher@gmail.com>
2013-10-17 08:25:34 -0700

    Updates setup.py to indicate Python 3 compatibility.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
+import sys
+
+if sys.version_info[0] == 3:
+    LONG_DESCRIPTION = open('README.txt', encoding='utf-8').read()
+else:
+    LONG_DESCRIPTION = open('README.txt').read()
 
 setup(
     name='prettyplotlib',
@@ -16,10 +22,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Scientific/Engineering'
     ],
     description='Painlessly create beautiful default `matplotlib` plots.',
-    long_description=open('README.txt').read(),
+    long_description=LONG_DESCRIPTION,
     install_requires=['matplotlib >= 1.2.1',
               'brewer2mpl >= 1.3.1']
 )
