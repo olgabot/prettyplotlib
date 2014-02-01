8fd62b820cb03b1dcfc3945f612ca43f916b86a2
Jan Gosmann <jan@hyper-world.de>
2014-01-31 20:16:11 -0500

    Fix eventplot for 1d arguments.

--- prettyplotlib/_eventplot.py
+++ prettyplotlib/_eventplot.py
@@ -1,5 +1,7 @@
 __author__ = 'jgosmann'
 
+from matplotlib.cbook import iterable
+
 from prettyplotlib.utils import remove_chartjunk, maybe_get_ax
 from prettyplotlib.colors import set2
 
@@ -8,12 +10,17 @@ def eventplot(*args, **kwargs):
     ax, args, kwargs = maybe_get_ax(*args, **kwargs)
     show_ticks = kwargs.pop('show_ticks', False)
 
-    # FIXME 1d positions
     if len(args) > 0:
         positions = args[0]
     else:
         positions = kwargs['positions']
-    kwargs.setdefault('colors', [c + (1.0,) for c in set2[:len(positions)]])
+
+    if any(iterable(p) for p in positions):
+        size = len(positions)
+    else:
+        size = 1
+
+    kwargs.setdefault('colors', [c + (1.0,) for c in set2[:size]])
 
     event_collections = ax.eventplot(*args, **kwargs)
     remove_chartjunk(ax, ['top', 'right'], show_ticks=show_ticks)
