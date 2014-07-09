bd8f9f93affc5b826697c64107d5e59f6769c7a6
Jan Domanski <jandom@gmail.com>
2014-07-09 21:07:17 +0100

    Enhancement: boxplot2

---
+++ sandbox/plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import prettyplotlib as ppl
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+np.random.seed(123)
+data1 = np.random.normal(loc=1, size=(6,6), scale=0.2)
+data2 = np.random.normal(size=(6,6), scale=0.2)
+ppl.boxplot2([data1, data2], colors=[ppl.colors.set1[1], ppl.colors.set1[2]], xticklabels=["data1", "data2"])
+#ppl.boxplot2(data2, color=ppl.colors.set1[2])
+ax.set_ylabel("Mean")
+fig.savefig("plot.png")
