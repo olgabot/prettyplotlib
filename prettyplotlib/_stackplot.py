de850aa65196d377f0e8cf8de005b250b549ed5b
Francis Colas <francis.colas@mavt.ethz.ch>
2014-07-25 16:06:49 +0200

    removed unnecessary color handling

--- prettyplotlib/_stackplot.py
+++ prettyplotlib/_stackplot.py
@@ -8,12 +8,7 @@ def stackplot(*args, **kwargs):
     lw = maybe_get_linewidth(**kwargs)
     kwargs['linewidths'] = lw
 
-    if 'colors' not in kwargs:
-        # if no color is specified, cycle over the ones in this axis
-        color_cycle = ax._get_lines.color_cycle
-        kwargs['color'] = next(color_cycle)
     kwargs.setdefault('edgecolor', almost_black)
-
     show_ticks = kwargs.pop('show_ticks', False)
 
     lines = ax.stackplot(*args, **kwargs)
