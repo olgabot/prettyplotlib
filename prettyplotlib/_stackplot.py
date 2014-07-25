804041f97e81cedf3c64e87bd31bb0fdfd013127
Francis Colas <francis.colas@mavt.ethz.ch>
2014-07-25 15:58:12 +0200

    adding stackplot to prettyplotlib

---
+++ prettyplotlib/_stackplot.py
@@ -0,0 +1,21 @@
+from prettyplotlib.utils import remove_chartjunk, maybe_get_ax, maybe_get_linewidth
+from prettyplotlib.colors import almost_black, pretty
+
+@pretty
+def stackplot(*args, **kwargs):
+    ax, args, kwargs = maybe_get_ax(*args, **kwargs)
+
+    lw = maybe_get_linewidth(**kwargs)
+    kwargs['linewidths'] = lw
+
+    if 'colors' not in kwargs:
+        # if no color is specified, cycle over the ones in this axis
+        color_cycle = ax._get_lines.color_cycle
+        kwargs['color'] = next(color_cycle)
+    kwargs.setdefault('edgecolor', almost_black)
+
+    show_ticks = kwargs.pop('show_ticks', False)
+
+    lines = ax.stackplot(*args, **kwargs)
+    remove_chartjunk(ax, ['top', 'right'], show_ticks=show_ticks)
+    return lines 
